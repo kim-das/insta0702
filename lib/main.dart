910333620fcb579aae35c1e8c00c8fb2ada791a1
daskim <ewtk2024@gmail.com>
2024-07-01 17:02:32 +0900

    bus 숙제 전

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import './style.dart' as style;
+
+void main() {
+  runApp(
+      MaterialApp(
+          theme: style.theme,
+          home:MyApp()
+  ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Instagram'),
+        actions: [
+          Icon(Icons.add_box_outlined)
+        ],
+      ),
+      body:[Text('홈페이지'), Text('샵페이지')][tab],
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+
+        onTap: (i){
+          setState(() {
+              tab=i;
+            });
+          },
+
+        items: [
+          BottomNavigationBarItem(icon:Icon(Icons.home_outlined),label:'홈'),
+          BottomNavigationBarItem(icon:Icon(Icons.shopping_bag_outlined), label:'샵'),
+      ]),
+    );
+  }
+}
+
+
